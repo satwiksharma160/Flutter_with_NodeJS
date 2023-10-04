1971322db4e6429ff212eb05e2eefb0d86f4f246
Satwik <112326512+satwiksharma160@users.noreply.github.com>
2023-10-05 04:37:20 +0530

    Delete flutter frontend directory

--- flutter frontend/android/app/src/main/kotlin/com/example/backend_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.backend_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter frontend/lib/model/product_model.dart
+++
@@ -1,8 +0,0 @@
-class Product {
-  final String? id;
-  final String? name;
-  final String? price;
-  final String? desc;
-
-  Product({this.name, this.price, this.desc, this.id});
-}
